47490fe2810ff70c322075c4903c7e6e7df4dddd
Mario Manno <mario.manno@suse.com>
2021-02-11 15:42:11 +0100

    Patch nats to bind to local addr
    spec.address is not guaranteed to be a local interface.
    It is used to reference the pod, via a service.

---
+++ bosh/releases/pre_render_scripts/nats/nats-tls/jobs/patch_nats-tls.conf.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+target="/var/vcap/all-releases/jobs-src/nats/nats-tls/templates/nats-tls.conf.erb"
+sentinel="${target}.patch_sentinel"
+if [[ -f "${sentinel}" ]]; then
+  if sha256sum --check "${sentinel}" ; then
+    echo "Patch already applied. Skipping"
+    exit 0
+  fi
+  echo "Sentinel mismatch, re-patching"
+fi
+
+# Fix bind address of nats server
+patch --verbose "${target}" <<'EOT'
+@@ -41,7 +41,7 @@
+   end
+ %>
+
+-net: "<%= spec.address %>"
++net: "<%= ENV['HOSTNAME'] %>"
+ port: <%= p("nats.port") %>
+ prof_port: <%= p("nats.prof_port") %>
+ monitor_port: <%= p("nats.monitor_port") %>
+@@ -104,3 +104,4 @@ cluster {
+     <% end %>
+   ]
+ }
++
+EOT
+
+sha256sum "${target}" > "${sentinel}"
